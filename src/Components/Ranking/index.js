260e0576c661ea0360f2d4a14ffea1cca0e588b5
charlesmuller <charlesmuller@rede.ulbra.br>
2021-11-21 20:52:56 -0300

    Ajuste tela ranking

--- src/Components/Ranking/index.js
+++ src/Components/Ranking/index.js
@@ -6,7 +6,9 @@ function Ranking(){
 
     return(
         <div>
-            <h2>Ranking</h2>
+            <h2>Exemplo de Ranking</h2><br/>
+            <h3>Primeiro da lista</h3><br/>
+            <h3>Segundo da lista</h3>
         </div>
     );
 
