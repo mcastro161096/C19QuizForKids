b8db875e0da47dbb0c7ec8eafb4c69c6ef55fb1a
dsfgdfg <MATHEUSCASTRO161096@gmail.com>
2021-11-21 17:17:29 -0300

    Criando ButtonStartGame.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import "./App.css";
+import {Switch, Route} from 'react-router-dom';
 
 import Appbar from './Components/Login/UI/Appbar/Index';
-
+import ButtonStartGame from './Components/ButtonStartGame';
 
 function App() {
 
@@ -13,7 +14,16 @@ function App() {
   <Appbar/>
        <div className="container">
     
-        
+       <div className="row">
+            <div className="col">
+              <Switch>
+                <Route exact path="/" component={ButtonStartGame} />
+                {/* <Route path="/dashboard-com-react" component={Resumo} />
+                <Route path="/consultas" component={Consultas} />
+                <Route path="/faturamento" component={Faturamento} /> */}
+              </Switch>
+            </div>
+          </div>
         
       </div>
   
