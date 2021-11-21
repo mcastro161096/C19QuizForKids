07f2cf9a5b5ff6d5734f06b04b0f1ee901834d29
dsfgdfg <MATHEUSCASTRO161096@gmail.com>
2021-11-21 19:19:27 -0300

    Criando post do nickname.

--- src/Components/NickName/index.js
+++ src/Components/NickName/index.js
@@ -1,15 +1,40 @@
-import React from "react";
+import {React, useState} from "react";
 import {BsFillCaretRightSquareFill} from 'react-icons/bs'
 import {Link} from "react-router-dom";
+import axios from "axios";
+
 
 
 function NickName() {
+     const [nick, setnick] = useState("");
+
+   const handleChange = event => {
+        setnick({ nick: event.target.value });
+      }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const user = {
+          nick: nick
+        };
+    
+        axios.post(`https://localhost:44335/NickName/Create`, nick)
+          .then(res => {
+            console.log(res);
+            console.log(res.data);
+          })
+    }
     return (
+        <form onSubmit={handleSubmit}>
         <div>
-        <input type="text"></input> 
+        
+        <input type="text" name="nick" placeholder="Nickname"></input> 
       
-       <Link  to="/card"> <button className="btn btn-primary"> <BsFillCaretRightSquareFill/></button></Link>
+        <button type="submit" className="btn btn-primary"> <BsFillCaretRightSquareFill/></button>
+        {/* <Link  to="/card"></Link> */}
         </div>
+        </form>
     );
 }
 
