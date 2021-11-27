42e121da2559fcd060dc4d531984b7542b58549c
charlesmuller <charlesmuller@rede.ulbra.br>
2021-11-27 15:35:43 -0300

    Inserção do botão de voltar da tela ranking para inicio

--- src/Components/Ranking/index.js
+++ src/Components/Ranking/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import axios from "axios";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom"
+import "../Ranking/ranking.css"
 
 function Ranking(){
 
     return(
         <div>
-            <h2>Exemplo de Ranking</h2><br/>
-            <h3>Primeiro da lista</h3><br/>
-            <h3>Segundo da lista</h3>
+            <div>
+                <h2>Exemplo de Ranking</h2><br/>
+                <h3>Primeiro da lista</h3><br/>
+                <h3>Segundo da lista</h3>
+            </div>
+        
+            <div className="">
+                <Link to="/nickname">
+                    {" "}
+                    <button className="btnvoltar" >Voltar</button>
+                </Link>
+            </div>
+
         </div>
+        
+        
     );
 
 }
