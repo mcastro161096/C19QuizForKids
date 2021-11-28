f831bccf2f88fd31d3beffa9dbedbdb1d2ee0ecd
dsfgdfg <MATHEUSCASTRO161096@gmail.com>
2021-11-28 15:17:37 -0300

    Ajuste AppBar.

--- src/Components/Appbar/index.js
+++ src/Components/Appbar/index.js
@@ -4,7 +4,8 @@ import react from "react";
 function Appbar(){
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <label className="navbar-brand">Dash</label>
+            <label className="navbar-brand" style={{marginLeft:"3%"}}>Quiz</label>
+            <label className="navbar-brand " style={{ marginLeft:"80%"}}>{localStorage.getItem('NickName') || ""}</label>
             <button className="navbar-toggler" type="button" data-toggle="colapse" data-target="#navbarNav">
                 <span className="navbar-toggler-icon"></span>
             </button>
